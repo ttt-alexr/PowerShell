3eea1a01a15f54d2a581cd4bf3536fc27db233bb
Alex Ruby <alex.ruby@trustedtechteam.com>
2025-02-03 09:38:23 -0800

    working version of inactive users

--- inactiveusersdraft.ps1
+++ inactiveusersdraft.ps1
@@ -2,22 +2,54 @@
 # Install-Module Microsoft.Graph -Scope CurrentUser
 
 # Import the Microsoft.Graph module
-Import-Module Microsoft.Graph
+# Import-Module Microsoft.Graph
 
 # Connect to Microsoft Graph
-Connect-MgGraph -Scopes "User.Read.All"
+# Connect-MgGraph -Scopes "User.Read.All", ""
 
-# Get the current date and calculate the date 30 days ago
-$currentDate = Get-Date
-$thirtyDaysAgo = $currentDate.AddDays(-30)
+$users = Get-MgUser -All -Property DisplayName,SignInActivity,UserPrincipalName
 
-# Get all users
-$users = Get-MgUser -All
+$results = @()
 
-# Filter users who haven't logged in in the last 30 days
-$inactiveUsers = $users | Where-Object {
-    $_.SignInActivity.LastSignInDateTime -lt $thirtyDaysAgo
+foreach ($user in $users) {
+    try {
+        if ($user.SignInActivity) {
+            $lastSignIn = $user.SignInActivity.LastSuccessfulSignInDateTime
+
+            if ($lastSignIn) {
+                # Calculate the date 30 days ago
+                $thirtyDaysAgo = (Get-Date).AddDays(-30)
+
+                # Check if the last sign-in was more than 30 days ago
+                if ($lastSignIn -lt $thirtyDaysAgo) { # Changed to -lt (less than)
+                    $results += [PSCustomObject]@{
+                        DisplayName = $user.DisplayName
+                        UserPrincipalName = $user.UserPrincipalName
+                        LastSuccessfulSignInDateTime = $lastSignIn
+                    }
+                }
+            } else {
+              #If LastSuccessfulSignInDateTime is null, treat it as older than 30 days.
+              $results += [PSCustomObject]@{
+                    DisplayName = $user.DisplayName
+                    UserPrincipalName = $user.UserPrincipalName
+                    LastSuccessfulSignInDateTime = "Never" 
+                }
+            }
+        } else {
+            # If SignInActivity is null, treat it as older than 30 days.
+            $results += [PSCustomObject]@{
+                DisplayName = $user.DisplayName
+                UserPrincipalName = $user.UserPrincipalName
+                LastSuccessfulSignInDateTime = "Never"
+            }
+        }
+
+    } catch {
+        Write-Warning "Error processing user $($user.UserPrincipalName): $_"
+    }
 }
 
-# Output the inactive users
-$inactiveUsers | Select-Object DisplayName, UserPrincipalName, SignInActivity.LastSignInDateTime
+# Filter the results *after* the loop to display only users older than 30 days.
+# This is more efficient than filtering inside the loop.
+$results | Where-Object {$_.LastSuccessfulSignInDateTime -ne "Never"} | Format-Table -AutoSize # Filtered results
